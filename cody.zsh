48b427e412fc2bcaac8a13aca5901fdfa2fc7f77
Cody <cody_28@hotmail.fr>
2022-01-27 22:46:01 +0100

    nvm

--- cody.zsh
+++ cody.zsh
@@ -1,6 +1,9 @@
 export COWPATH="$COWPATH:$HOME/.cowsay/cowfiles"
 export PATH=$HOME/bin:$PATH
-
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+y
 source ~/.zsh/alias.zsh
 source ~/.zsh/themes.zsh
 source ~/.zsh/plugins.zsh

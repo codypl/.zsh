f6024bb9abea38a79fc1bb2511139b906194c973
Cody <cody_28@hotmail.fr>
2022-01-27 23:24:52 +0100

    nvm lazy load

--- cody.zsh
+++ cody.zsh
@@ -1,12 +1,11 @@
 export COWPATH="$COWPATH:$HOME/.cowsay/cowfiles"
 export PATH=$HOME/bin:$PATH
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-y
+export NVM_LAZY_LOAD=true
+
 source ~/.zsh/alias.zsh
 source ~/.zsh/themes.zsh
 source ~/.zsh/plugins.zsh
 
 clear
 /etc/motd.sh
+
--- plugins.zsh
+++ plugins.zsh
@@ -1,3 +1,4 @@
+source ~/.zsh/plugins/zsh-nvm/zsh-nvm.plugin.zsh
 source ~/.zsh/plugins/alias-tips/alias-tips.plugin.zsh
 source ~/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
